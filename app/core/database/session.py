a121d2b719b993b93c2a16109f71876838601063
Lakshyapandit-14 <lakshyapandit5599@gmail.com>
2025-10-16 19:16:26 +0530

    empty main branch

--- app/core/database/session.py
+++
@@ -1,9 +0,0 @@
-from app.core.connections.database import SessionLocal
-
-# Dependency — gets a database session for each request
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
