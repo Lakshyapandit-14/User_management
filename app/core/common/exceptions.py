4c8124a063c97eb620b589547a938832bc540af0
Lakshyapandit-14 <lakshyapandit5599@gmail.com>
2025-10-17 11:05:56 +0530

    revert last 3 commit

---
+++ app/core/common/exceptions.py
@@ -0,0 +1,28 @@
+from fastapi import HTTPException, status
+
+# --- Custom Exception Classes ---
+
+class UserNotFoundException(HTTPException):
+    def __init__(self, detail: str = "User not found"):
+        super().__init__(status_code=status.HTTP_404_NOT_FOUND, detail=detail)
+
+
+class UserAlreadyExistsException(HTTPException):
+    def __init__(self, detail: str = "User already exists"):
+        super().__init__(status_code=status.HTTP_400_BAD_REQUEST, detail=detail)
+
+
+class InvalidCredentialsException(HTTPException):
+    def __init__(self, detail: str = "Invalid username or password"):
+        super().__init__(status_code=status.HTTP_401_UNAUTHORIZED, detail=detail)
+
+
+class UnauthorizedException(HTTPException):
+    def __init__(self, detail: str = "You are not authorized to perform this action"):
+        super().__init__(status_code=status.HTTP_403_FORBIDDEN, detail=detail)
+
+
+# --- Optional Helper Function ---
+def raise_http_exception(status_code: int, message: str):
+    """Generic HTTP exception raiser"""
+    raise HTTPException(status_code=status_code, detail=message)
