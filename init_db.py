d3365cc36fded7516a4783a59cccfa3eed02895d
Lakshyapandit-14 <lakshyapandit5599@gmail.com>
2025-10-17 11:26:34 +0530

    emptied main file

--- init_db.py
+++
@@ -1,6 +0,0 @@
-from app.core.connections.database import Base, engine
-from app.services.users.models import User
-from app.services.users.profile.models import UserProfile
-
-Base.metadata.create_all(bind=engine)
-print("Tables created!")
