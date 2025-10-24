94306dab90c057d09a97b972fa4224a8a5dececb
lakshya pandit <147169519+Lakshyapandit-14@users.noreply.github.com>
2025-10-24 05:56:25 +0000

    comment out the Middleware code from main file

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ from app.core.config import settings
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
 from app.core.connections.database import engine, Base
+from app.api.user.v1 import profile_router
 
 # Create FastAPI app
 app = FastAPI(
@@ -15,21 +16,21 @@ app = FastAPI(
 )
 
 # Enable CORS (Cross-Origin Resource Sharing)
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # You can restrict this later to your frontend domain
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-from app.api.user.v1 import profile_router # now (19/10/2025 : 9:33 am)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],  # You can restrict this later to your frontend domain
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+ # now (19/10/2025 : 9:33 am)
 # Include versioned routers
 app.include_router(user_router, prefix="/api/v1/users", tags=["Users"])
 app.include_router(auth_router, prefix="/api/v1/auth", tags=["Auth"])
 #app.include_router(profile_router, prefix="/api/v1/user", tags=["Profile"])
 
 # Health check endpoint
-@app.get("/", tags=["Health"])
+@app.get("/", tags=["Demo"])
 def health_check():
     return {"status": "ok", "message": "User Management Microservice is running 🚀"}
 
